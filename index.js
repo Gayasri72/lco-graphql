92c70dc42ec1bcb5eb530d3fce716c3cdec63f0e
Gayasri72 <gayasripethum1@gmail.com>
2025-06-16 17:03:37 +0530

    complete the crash course for learn graphql

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Start your es6 scripts here
+import express from 'express';
+import resolvers from './resolvers.js';
+import schema from './schema.js';
+import { graphqlHTTP } from 'express-graphql';
+const app= express();
+
+app.get ('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const root = resolvers
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(8082, () => {
+  console.log('Server is running on http://localhost:8082');
+});
